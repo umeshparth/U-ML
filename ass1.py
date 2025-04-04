570a8d5158449c4324b5d2d70e53974b9b1e44f6
umeshparth <126247728+umeshparth@users.noreply.github.com>
2025-04-04 08:15:44 +0530

    Update ass1.py

--- ass1.py
+++ ass1.py
@@ -8,3 +8,19 @@ Hello *world!*
  
 df = pd.read_csv("my_data.csv")
 st.line_chart(df)
+
+st.title("Hello Streamlit-er 👋")
+st.markdown(
+    """ 
+    This is a playground for you to try Streamlit and have fun. 
+
+    **There's :rainbow[so much] you can build!**
+    
+    We prepared a few examples for you to get started. Just 
+    click on the buttons above and discover what you can do 
+    with Streamlit. 
+    """
+)
+
+if st.button("Send balloons!"):
+    st.balloons()
